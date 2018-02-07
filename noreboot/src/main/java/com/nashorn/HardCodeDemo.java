a160673efafaf4e177ebb151b2bee97da9056966
phideon <a0z0to9@outlook.com>
2018-02-07 13:08:18 +0800

    solution two: hard code change.

--- noreboot/src/main/java/com/nashorn/HardCodeDemo.java
+++ noreboot/src/main/java/com/nashorn/HardCodeDemo.java
@@ -3,53 +3,49 @@ package com.nashorn;
 import com.nashorn.util.Constant;
 import com.nashorn.util.EngineLoadUtils;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.nio.charset.Charset;
-import java.util.Scanner;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 /**
  * @author i324779
  *         No need to restart the applicion or server,
- *         we can change the element in the formula,
- *         and get different result.
+ *         sometimes, we need hard code some configuration,
+ *         and need to change them.
  *         <p>
  *         here use below function in the model.js:
- *         function formula(var1, var2) {
- *         return var1 + var2 + factor + 5;
+ *         function allIds() {
+ *         return "1,2,3,4,5,6";
  *         }
- *         if the constant 5 need be be change frequently,
- *         here is a good solution.
+ *         now I want to add some ids such as "7,8",
+ *         just change the string ids is fine.
  */
-public class FormulaModificationDemo {
-
+public class HardCodeDemo {
     public static void main(String[] args)
             throws FileNotFoundException, ScriptException, NoSuchMethodException {
 
         ScriptEngine engine = EngineLoadUtils.loadNashornEngine();
-        // here set a element in the formula, no need modify.
-        engine.put("factor", 0);
 
         // user Scanner to wait user input and application keeping running.
         // you can modify the formula function,
         // and re-input numbers, can get different result.
         try (Scanner input = new Scanner(System.in)) {
-            System.out.println("Please input two numbers:");
+            System.out.println("Please input one number:");
             while (input.hasNextInt()) {
-                int first = input.nextInt();
-                int second = input.nextInt();
-                System.out.printf("Input number are: %d, %d%n", first, second);
+                int number = input.nextInt();
+
+                System.out.printf("Input number is: %d%n", number);
 
                 engine.eval(new FileReader(Constant.JAVASCRIPT_FUNCTION_PATH));
                 if (engine instanceof Invocable) {
                     Invocable in = (Invocable) engine;
                     // execute javascript function in the model.js file.
-                    Double result = (Double) in.invokeFunction("formula", first, second);
+                    String result = (String) in.invokeFunction("allIds");
 
-                    System.out.printf("The result is %d%n", result.intValue());
+                    System.out.printf("The result is %s%n", result);
                 }
             }
         }
